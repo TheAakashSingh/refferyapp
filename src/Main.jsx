a91653363ff0f06e7735428bcfb9ba6d1a1dd0c0
Aakash <99164199+TheAakashSingh@users.noreply.github.com>
2024-04-06 17:09:03 +0530

    dsdsd

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
 import Marquee from "react-fast-marquee";
 import img1 from "./Media/img1.png";
 import img2 from "./Media/Bandar.png";
 import star from "./Media/Star 34.png";
 import suf1 from "./Media/_.png";
+import twitter from "./Media/Behance.png";
+import Telegram from "./Media/Telegram.png";
 import img3 from "./Media/img2.png";
+
 const Main = () => {
   return (
     <div className="mainPage">
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+      </div>
       <div className="sec1">
         <span>Jeffrey Epstein</span>
       </div>
-
       <div className="sec2">
         <div className="marq">
           <Marquee>
@@ -57,7 +71,6 @@ const Main = () => {
           <img src={img2} />
         </div>
       </div>
-
       <div className="sec4">
         <div className="marq">
           <Marquee>
@@ -96,19 +109,46 @@ const Main = () => {
           </Marquee>
         </div>
       </div>
-
       <div className="sec5">
         <p>Token Address:01xhgjhgd6567672hjhj</p>
         <span>Total Supply : 673,343,43</span>
         <span>Liquidity burnt 🔥 Contract Renounced 🔒</span>
         <button type="button">Swap Now</button>
       </div>
-
       <div className="sec6">
         <img src={img3} />
         <span>Grap The Jeffrey</span>
         <img src={img3} />
       </div>
+      <div className="sec7">
+        <p>Discover My Secret</p>
+        <div className="boxes">
+          <div className="box box-a"></div>
+          <div className="box box-b"></div>
+          <div className="box box-a"></div>
+
+          <div className="box box-a"></div>
+          <div className="box box-b"></div>
+          <div className="box box-a"></div>
+
+          <div className="box box-a"></div>
+          <div className="box box-b"></div>
+          <div className="box box-a"></div>
+
+          <div className="box box-a"></div>
+          <div className="box box-b"></div>
+          <div className="box box-a"></div>
+
+          <div className="box box-a"></div>
+          <div className="box box-b"></div>
+          <div className="box box-a"></div>
+        </div>
+      </div>
+      <div className="sec8">
+        <img src={Telegram} />
+        <img src={twitter} />
+      </div>
+      <footer>© 2022 Biccas Inc. Copyright and rights reserved</footer>
     </div>
   );
 };
